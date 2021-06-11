682211e7e7c676347bd8eca7069105deafe0d316
Pradhumn Dave <ddave2001@gmail.com>
2021-06-11 22:08:52 +0530

    Infinity community is fully functional

--- src/app/customComponents/posts/post.model.ts
+++ src/app/customComponents/posts/post.model.ts
@@ -2,29 +2,32 @@ export class Post {
   public id: any;
   public img: string;
   public text: string;
-  public supports: number;
+  public supportCount: any;
   public owner: string;
   public onlyMe: boolean;
   public userProfileImage: any;
   public userName: any;
+  public createdAt: any;
 
   constructor(
     id: any,
     img: string,
     text: string,
-    supports: number,
+    supportCount: any,
     owner: string,
     onlyMe: boolean,
     userProfileImage: any,
-    userName: any
+    userName: any,
+    createdAt: any
   ) {
     this.id = id;
     this.img = img;
     this.text = text;
-    this.supports = supports;
+    this.supportCount = supportCount;
     this.owner = owner;
     this.onlyMe = onlyMe;
     this.userProfileImage = userProfileImage;
     this.userName = userName;
+    this.createdAt = createdAt;
   }
 }
