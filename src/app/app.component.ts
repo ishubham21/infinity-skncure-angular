15de09cbf60ca541b7bb7303f37b17f1ba2eab93
Srashti Jain <srashti0204@gmail.com>
2021-06-23 20:12:27 +0530

    Added navbar to appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infinity-skncure';
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {}
 }
